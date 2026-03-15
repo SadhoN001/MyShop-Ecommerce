d410291a078c1d7797b9420ce05363f816f86e9e
Sadhon Kumar Dev <skdsadhon@gmail.com>
2026-03-15 13:30:05 +0600

    Add whitenoise for static files

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware", 
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',# static file for deployment 
     # Add the account middleware:
     "allauth.account.middleware.AccountMiddleware",
 ]
