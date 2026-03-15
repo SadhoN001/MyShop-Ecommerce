0841cf6b2ccf6b86648945eb94b95a4e57e6ec95
Sadhon Kumar Dev <skdsadhon@gmail.com>
2026-03-15 13:09:09 +0600

    Fix allowed hosts

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
